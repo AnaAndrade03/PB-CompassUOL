a3d3e50e2de308962325e371b47f6bcd95fa56ac
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 20:50:23 -0300

    11

--- Sprint_2/Exercícios/exercicio_11.sql
+++ Sprint_2/Exercícios/exercicio_11.sql
@@ -1,7 +1,7 @@
 E11
 Apresente a query para listar o código e nome cliente com maior gasto na loja.
-As colunas presentes no resultado devem ser cdcli, nmcli e gasto, esta última representando o somatório das vendas (concluídas) atribuídas 
-ao cliente.
+As colunas presentes no resultado devem ser cdcli, nmcli e gasto, esta última representando o somatório das vendas (concluídas) 
+atribuídas ao cliente.
 
 select 
   cdcli,
