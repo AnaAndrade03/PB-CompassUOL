adc0d34b4e03174f00d8949bf3079b09296103cf
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 20:42:45 -0300

    4

--- Sprint_2/Exercícios/exercicio_04.sql
+++ Sprint_2/Exercícios/exercicio_04.sql
@@ -1,1 +1,17 @@
+E04
+Apresente a query para listar a quantidade de livros publicada por cada autor. 
+Ordenar as linhas pela coluna nome (autor), em ordem crescente. 
+Além desta, apresentar as colunas codautor, nascimento e quantidade (total de livros de sua autoria).
 
+  select 
+	aut.nome,
+	aut.codautor,
+	aut.nascimento,
+	count (li.autor) as quantidade
+
+from autor as aut
+left join livro as li
+ on aut.codautor = li.autor
+
+Group by aut.nome
+order by aut.nome
