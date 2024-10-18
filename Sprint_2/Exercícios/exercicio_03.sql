d0cc947188123a48d229b685a1805ed264e250d9
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 20:41:00 -0300

    3

--- Sprint_2/Exercícios/exercicio_03.sql
+++ Sprint_2/Exercícios/exercicio_03.sql
@@ -1,1 +1,17 @@
+E03
+ Apresente a query para listar as 5 editoras com mais livros na biblioteca. O resultado deve conter apenas as colunas quantidade, nome, estado e cidade.
+Ordenar as linhas pela coluna que representa a quantidade de livros em ordem decrescente.
 
+select
+    count (l.editora) as quantidade,
+    e.nome,
+    end.estado,
+    end.cidade
+
+from livro as l
+    left join editora as e
+    on l.editora = e.codeditora
+    left join endereco as end
+    on e.endereco = end.codendereco
+group by l.editora 
+order by quantidade desc
