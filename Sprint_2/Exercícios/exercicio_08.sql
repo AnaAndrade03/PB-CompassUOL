9068e8b85f728a44cc4117343f24cb7efe7264c3
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 20:46:25 -0300

    8

--- Sprint_2/Exercícios/exercicio_08.sql
+++ Sprint_2/Exercícios/exercicio_08.sql
@@ -1,1 +1,17 @@
+E08
+Apresente a query para listar o código e o nome do vendedor com maior número de vendas (contagem), e que estas vendas estejam com o 
+status concluída. As colunas presentes no resultado devem ser, portanto, cdvdd e nmvdd.
 
+select 
+	vendedor.cdvdd,
+    vendedor.nmvdd
+
+from tbvendedor as vendedor
+left join tbvendas as vendas 
+on vendedor.cdvdd = vendas.cdvdd
+
+where vendas.status like 'Concluído'
+group by vendas.cdvdd 
+having count (vendas.cdvdd) >= 1
+order by count (vendas.cdvdd) DESC
+limit 1
