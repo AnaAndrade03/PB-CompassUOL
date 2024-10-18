02b48c2a589f45df35f221890f17371bbf290d6b
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 20:45:17 -0300

    7

--- Sprint_2/Exercícios/exercicio_07.sql
+++ Sprint_2/Exercícios/exercicio_07.sql
@@ -1,1 +1,10 @@
+E07
+Apresente a query para listar o nome dos autores com nenhuma publicação. Apresentá-los em ordem crescente.
 
+select 
+ aut.nome
+
+from autor as aut
+left join livro as li 
+on aut.codautor = li.autor
+where cod is NULL
