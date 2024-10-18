fdd90594ad99f961760e2b9b363942bcb2d0d0b4
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 20:44:34 -0300

    6

--- Sprint_2/Exercícios/exercicio_06.sql
+++ Sprint_2/Exercícios/exercicio_06.sql
@@ -1,1 +1,16 @@
+E06
+Apresente a query para listar o autor com maior número de livros publicados. 
+O resultado deve conter apenas as colunas codautor, nome, quantidade_publicacoes.
 
+select 
+	aut.codautor,
+    aut.nome,
+    (count (li.autor)) as quantidade_publicacoes
+    
+    FROM autor as aut
+left join livro as li 
+on aut.codautor = li.autor
+    
+group by li.autor 
+order by quantidade_publicacoes desc
+limit 1
