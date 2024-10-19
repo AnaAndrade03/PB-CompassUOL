a73be7f0b6d78d42f31b2d88917bbf0748584e24
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-19 11:56:55 -0300

    ex

--- Sprint_2/Desafio/explicando_resolução.sql
+++ Sprint_2/Desafio/explicando_resolução.sql
@@ -1,1 +1,5 @@
-
+Resolução desafio SQL
+ 1- Ao receber a tabela, fui aplicando as normalizações criando as tabelas "carro", "cliente", "combustivel", "locação" e "vendedor".
+ 2- Ao inserir os dados nas tabelas novas usei apanas a quilometragem mais atual a colocando na tabela "carro" a julgando como 
+ característica do carro.
+ 3- Para a modelagem dim ensional fiz uso das views para viaulização das tabelas dimensão e fato.
