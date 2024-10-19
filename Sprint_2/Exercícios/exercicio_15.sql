dcbfb6c0af3bd12cdc91461dd02cde30922c6fce
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 21:04:14 -0300

    15

--- Sprint_2/Exercícios/exercicio_15.sql
+++ Sprint_2/Exercícios/exercicio_15.sql
@@ -1,1 +1,8 @@
+E15
+Apresente a query para listar os códigos das vendas identificadas como deletadas. Apresente o resultado em ordem crescente.
 
+select 
+	cdven
+
+from tbvendas    
+where deletado = 1 
