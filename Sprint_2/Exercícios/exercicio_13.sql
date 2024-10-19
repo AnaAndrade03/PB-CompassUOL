8a5a87acd270c4eb0f04c145b5a4b0221d7a7be3
Ana Carolina Oliveira de Andrade <andrade.ana03@gmail.com>
2024-10-18 21:02:41 -0300

    13

--- Sprint_2/Exercícios/exercicio_13.sql
+++ Sprint_2/Exercícios/exercicio_13.sql
@@ -1,1 +1,14 @@
+E13
+Apresente a query para listar os 10 produtos menos vendidos pelos canais de E-Commerce ou Matriz (Considerar apenas vendas concluídas).  
+As colunas presentes no resultado devem ser cdpro, nmcanalvendas, nmpro e quantidade_vendas.
 
+select 
+	cdpro,
+    nmcanalvendas,
+    nmpro,
+    sum(qtd) as quantidade_vendas
+from tbvendas
+where status = 'Concluído'
+GROUP BY cdpro, nmcanalvendas, nmpro
+order by quantidade_vendas
+limit 10
