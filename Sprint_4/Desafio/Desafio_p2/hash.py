90675f375290019332076cf9691802935f9a6c1b
andrad <andrade.ana03@gmail.com>
2024-11-18 19:49:06 -0300

    editfinish

--- Sprint_4/Desafio/Desafio_p2/hash.py
+++ Sprint_4/Desafio/Desafio_p2/hash.py
@@ -1,14 +1,13 @@
 import hashlib
 while True:
-    its_hashed = hashlib.sha1()
     descubra = input("Está pronto para uma aventura no mundo das hashes? Digite algo e veja o que acontece... (ou 'sair' para escapar do desafio!)")
     
     if descubra.lower() == "sair":
         print("Você conseguiu escapar do desafio... Mas será que você encontrou todas as respostas? Até a próxima, explorador!")
         break
-
-    its_hashed.update(descubra.encode())
-    hash_hex = its_hashed.hexdigest()
+    
+    hash_hex = hashlib.sha1(descubra.encode()).hexdigest()
+    
     if len(descubra) < 5:
         print(f'Isso foi mais fácil que escalar uma montanha de marshmallows! Sua hash de: {descubra} é: {hash_hex}')
     else:
