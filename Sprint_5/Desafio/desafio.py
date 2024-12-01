f2e7c98ed2c43ef6ca0140c8e259e49a70075b84
andrad <andrade.ana03@gmail.com>
2024-12-01 11:47:17 -0300

    edit

--- Sprint_5/Desafio/desafio.py
+++ Sprint_5/Desafio/desafio.py
@@ -1,13 +1,25 @@
 #bens tombados nas decadas de 70, 80 e 90 que se encaixam nas categorias bem imóvel e conjunto paisagístico, mostrando o número de tombamentos por municipio e a quantos anos ocorreu o tombamento.
-
 #imports
 import boto3
 import pandas as pd
 from datetime import datetime
+from io import StringIO
 
-session = boto3.Session(profile_name='default')
+#lendo csv do bucket
+session = boto3.Session(profile_name='Ana')
+s3_cliente = session.client('s3')
 
-df = pd.read_csv(r'C:\Users\andra\OneDrive\Área de Trabalho\desafio\processos-tombo.csv', sep=';', on_bad_lines='skip')
+bucket_name = 'desafiodados-sprint6'
+s3_key = 'processos-tombo.csv'
+
+try:
+    response = s3_cliente.get_object(Bucket=bucket_name, Key=s3_key)
+    csv_content = response['Body'].read().decode('utf-8') 
+    df = pd.read_csv(StringIO(csv_content), delimiter=';', on_bad_lines='skip')
+    print("Arquivo lido do S3 com sucesso!")
+except Exception as e:
+    print(f"Erro ao ler o arquivo do S3: {e}")
+    exit()
 
 #definindo função data
 ano_atual = datetime.now().year
@@ -24,14 +36,22 @@ filtro_1['está_em_bh?'] = filtro_1['municipio'].apply(lambda x: 'Sim' if x == '
 filtro_1['anos_desde_o_tombamento'] = ano_atual - filtro_1['ano']
 
 #dropando colunas 
-filtro_1.drop('denominacao_completa', axis=1, inplace=True)
-filtro_1.drop('ato_legal', axis=1, inplace=True)
-filtro_1.drop('livro_de_tombo', axis=1, inplace=True)
-filtro_1.drop('distrito', axis=1, inplace=True)
+filtro_1.drop(['denominacao_completa', 'ato_legal', 'livro_de_tombo', 'distrito'], axis=1, inplace=True)
 
-#transformando o resultado em um novo csv
-filtro_1.to_csv(r'C:\Users\andra\OneDrive\Área de Trabalho\desafio\tratamento_dados_bens_tombados.csv', index=False, sep=';')
+#transformando o resultado em um novo csv e salvando localmente 
+local_file_path = r'C:\Users\andra\OneDrive\Área de Trabalho\desafio\tratamento_dados_bens_tombados.csv'
+filtro_1.to_csv(local_file_path, index=False, sep=';')
+print(f"Arquivo processado salvo localmente em: {local_file_path}")
 
-#mandando para o bucket
-s3 = boto3.client('s3')
-s3.upload_file(r'C:\Users\andra\OneDrive\Área de Trabalho\desafio\tratamento_dados_bens_tombados.csv', 'desafiodados-sprint6', 'tratamento_dados_bens_tombados.csv')
+#mandando o para o bucket
+session = boto3.Session(profile_name='Ana')
+s3 = session.client('s3')
+
+local_file_path = r'C:\Users\andra\OneDrive\Área de Trabalho\desafio\tratamento_dados_bens_tombados.csv'
+bucket_name = 'desafiodados-sprint6'
+
+try:
+    s3.upload_file(local_file_path, bucket_name, 'tratamento_dados_bens_tombados.csv')
+    print("Arquivo enviado para o S3 com sucesso!")
+except Exception as e:
+    print(f"Erro ao enviar o arquivo para o S3: {e}")
--- Sprint_5/Desafio/enviandobucket.py
+++ Sprint_5/Desafio/enviandobucket.py
@@ -1,6 +1,14 @@
 import boto3 
 
-session = boto3.Session(profile_name='default')
+session = boto3.Session(profile_name='Ana')
+s3 = session.client('s3')
 
-s3 = boto3.client('s3')
-s3.upload_file(r'C:\Users\andra\OneDrive\Área de Trabalho\desafio\processos-tombo.csv', 'desafiodados-sprint6', 'processos-tombo.csv')
+local_file_path = r'C:\Users\andra\OneDrive\Área de Trabalho\desafio\processos-tombo.csv'
+bucket_name = 'desafiodados-sprint6'
+s3_key = 'processos-tombo.csv'
+
+try:
+    s3.upload_file(local_file_path, bucket_name, s3_key)
+    print(f"Arquivo '{local_file_path}' enviado com sucesso para o bucket '{bucket_name}' com a chave '{s3_key}'.")
+except Exception as e:
+    print(f"Erro ao enviar o arquivo para o S3: {e}")
